487138065825350bce0840145ad982947b4950cf
Mark Jones <mark@thisismarkup.co.uk>
2018-05-20 19:58:08 +0100

    Use newtype for wordlist for clarity

--- hangman/src/Main.hs
+++ hangman/src/Main.hs
@@ -7,7 +7,9 @@ import Data.List (intersperse)
 import System.Exit (exitSuccess)
 import System.Random (randomRIO)
 
-type WordList = [String]
+newtype WordList =
+  WordList [String]
+  deriving (Eq, Show)
 
 data Puzzle =
   Puzzle String [Maybe Char] [Char]
@@ -21,7 +23,7 @@ instance Show Puzzle where
 allWords :: IO WordList
 allWords = do
   dict <- readFile "data/dict.txt"
-  return (lines dict)
+  return $ WordList (lines dict)
 
 minWordLength :: Int
 minWordLength = 5
@@ -31,14 +33,14 @@ maxWordLength = 9
 
 gameWords :: IO WordList
 gameWords = do
-  aw <- allWords
-  return (filter gameLength aw)
+  (WordList aw) <- allWords
+  return $ WordList (filter gameLength aw)
   where gameLength w =
           let l = length (w :: String)
           in l >= minWordLength && l < maxWordLength
 
 randomWord :: WordList -> IO String
-randomWord wl = do
+randomWord (WordList wl) = do
   randomIndex <- randomRIO (0, l)
   return $ wl !! randomIndex
   where l = length wl - 1
