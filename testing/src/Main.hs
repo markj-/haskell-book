c5f5f9d6d613c1a7b0dac329ea0e0a95be7fed60
Mark Jones <mark@thisismarkup.co.uk>
2018-05-22 19:44:06 +0100

    Simple hspec and quickcheck examples

---
+++ testing/src/Main.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Test.Hspec
+import Test.QuickCheck
+
+main :: IO ()
+main = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
+  describe "QuickCheck" $ do
+    it "x + 1 is always\
+      \ greater than x" $ do
+        property $ \x -> x + 1 > (x :: Int)
